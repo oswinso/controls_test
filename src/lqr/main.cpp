8047cf0738b3881a71c91a78390a78662b60ced6
Oswin So <oswinso@gmail.com>
2019-09-02 01:01:18 -0400

    w00t it kind of works

--- src/lqr/main.cpp
+++ src/lqr/main.cpp
@@ -1,32 +1,35 @@
 #include <Eigen/Core>
-#include <iostream>
-#include <iomanip>
 #include <fstream>
+#include <iomanip>
+#include <iostream>
 
-constexpr double delta_t = 1e-1;
+constexpr double delta_t = 1e-2;
 constexpr double g = 9.80665;
 constexpr double l = 1.0;
-constexpr int timesteps = 10;
+constexpr int timesteps = 1000;
+constexpr double friction_coeff = 0.1;
+constexpr double max_torque = 100.0;
 
 using VectorVector2d = std::vector<Eigen::Vector2d, Eigen::aligned_allocator<Eigen::Vector2d>>;
 
+Eigen::Vector2d getDelta(const Eigen::Vector2d& state, double control)
+{
+  return { state(1), -g / l * sin(state(0) + M_PI) + control - friction_coeff * state(1) };
+}
+
 Eigen::Vector2d propogate(const Eigen::Vector2d& state, double control)
 {
-  std::cout << "state: " << state.transpose() << std::endl;
-  double theta = state(0);
-  double theta_dot = state(1);
+  Eigen::Vector2d k1 = getDelta(state, control);
+  Eigen::Vector2d k2 = getDelta(state + k1 * delta_t / 2.0, control);
+  Eigen::Vector2d k3 = getDelta(state + k2 * delta_t / 2.0, control);
+  Eigen::Vector2d k4 = getDelta(state + k3 * delta_t, control);
 
-  double theta_ddot = -g / l * sin(theta) + control;
-  std::cout << "theta_ddot: " << theta_ddot << std::endl;
-  theta += theta_dot * delta_t + 0.5 * theta_ddot * delta_t * delta_t;
-  theta_dot += theta_ddot * delta_t;
-
-  return {theta, theta_dot};
+  return state + (k1 + 2.0 * k2 + 2.0 * k3 + k4) / 6.0 * delta_t;
 }
 
 std::vector<double> getControl(const Eigen::Vector2d& x)
 {
-//  std::cout << ">> getControl" << std::endl;
+  //  std::cout << ">> getControl" << std::endl;
   // Define Variables
   double phi = x(0);
   Eigen::Vector3d state{};
@@ -36,7 +39,7 @@ std::vector<double> getControl(const Eigen::Vector2d& x)
   Eigen::Matrix3d a;
   // clang-format off
   a <<  1,                          delta_t,  0,
-        -g/l * cos(phi) * delta_t,  1,        -g/l * (sin(phi) - phi * cos(phi)),
+        g/l * cos(phi) * delta_t,  1,        g/l * (sin(phi) - phi * cos(phi)),
         0,                          0,        1;
   // clang-format on
 
@@ -44,12 +47,12 @@ std::vector<double> getControl(const Eigen::Vector2d& x)
 
   Eigen::Matrix3d q;
   // clang-format off
-  q <<  1e5,  0,    0,
-        0,    1e5,  0,
+  q <<  1e6,  0,    0,
+        0,    1e4,  0,
         0,    0,    0;
   // clang-format on
 
-  constexpr double r = 0.0;
+  constexpr double r = 1e1;
 
   Eigen::MatrixX3d ks(timesteps - 1, 3);
 
@@ -63,7 +66,7 @@ std::vector<double> getControl(const Eigen::Vector2d& x)
 
     Eigen::Matrix<double, 1, 3> k = -1 / q_uu * q_xu;
     ks.row(i) = k;
-//    std::cout << "row " << i << ": " << k << "\n";
+    //    std::cout << "row " << i << ": " << k << "\n";
 
     p = q_xx + q_xu.transpose() * k;
   }
@@ -71,11 +74,12 @@ std::vector<double> getControl(const Eigen::Vector2d& x)
   std::vector<double> controls;
   controls.reserve(timesteps - 1);
 
-//  std::cout << ">> forward step" << std::endl;
+  //  std::cout << ">> forward step" << std::endl;
   for (int i = 0; i < timesteps - 1; i++)
   {
-//    std::cout << "row " << i << ": " << ks.row(i) << "\n";
-//    std::cout << "row " << i << " * state: " << ks.row(i) * state << "\n";
+    //    std::cout << "row " << i << ": " << ks.row(i) << "\n";
+//    std::cout << "row " << i << "(" << ks.row(i) << ")"
+//              << " * state (" << state.transpose() << ") : " << ks.row(i) * state << "\n";
     double u = (ks.row(i) * state)(0);
     controls.emplace_back(u);
     state = a * state + b * u;
@@ -119,48 +123,51 @@ int main()
     {
       x0 = propogate(x0, 0.0);
       std::cout << "Control: " << 0.0 << ", state: (" << x0(0) << ", " << x0(1) << ")" << std::endl;
-      csv << 0.0 << "," << x0(0) <<"," << x0(1) << "\n";
+      csv << 0.0 << "," << x0(0) << "," << x0(1) << "\n";
     }
   }
-//
-//  std::cout << "\n";
-//  std::cout << "========================" << std::endl;
-//  std::cout << "      Executing all     " << std::endl;
-//  std::cout << "========================" << std::endl;
-//  {
-//    Eigen::Vector2d x0{ 1.0, 0.0 };
-//    std::vector<double> us = getControl(x0);
-//
-//    std::cout << "Starting: (" << x0(0) << ", " << x0(1) << ")" << std::endl;
-//    for (const auto u : us)
-//    {
-//      x0 = propogate(x0, u);
-//      std::cout << "Control: " << u << ", state: (" << x0(0) << ", " << x0(1) << ")" << std::endl;
-//    }
-//
-//  }
-//  std::cout << "\n";
-//  std::cout << "========================" << std::endl;
-//  std::cout << "Now executing first only" << std::endl;
-//  std::cout << "========================" << std::endl;
-//
-//  {
-//    Eigen::Vector2d x0 {1.0, 0.0};
-//    std::cout << "Starting: (" << x0(0) << ", " << x0(1) << ")" << std::endl;
-//
-//    for (int i = 0; i < timesteps - 1; i++)
-//    {
-//      std::vector<double> us = getControl(x0);
-//      double u = us[0];
-//      x0 = propogate(x0, u);
-//      std::cout << "Control: " << u << ", state: (" << x0(0) << ", " << x0(1) << ")" << std::endl;
-//    }
-//  }
 
-//  for (size_t i = 0; i < controls.size(); i++)
-//  {
-//    std::cout << "Control: " << controls[i] << ", state: (" << states[i](0) << ", " << states[i](1) << ")" << std::endl;
-//  }
+  //  std::cout << "\n";
+  //  std::cout << "========================" << std::endl;
+  //  std::cout << "      Executing all     " << std::endl;
+  //  std::cout << "========================" << std::endl;
+  //  {
+  //    std::ofstream csv("all.csv");
+  //    csv << "u,x0,x1\n";
+  //
+  //    Eigen::Vector2d x0{ 1.0, 0.0 };
+  //    std::vector<double> us = getControl(x0);
+  //
+  //    std::cout << "\nStarting: (" << x0(0) << ", " << x0(1) << ")" << std::endl;
+  //    for (const auto u : us)
+  //    {
+  //      x0 = propogate(x0, u);
+  //      std::cout << "Control: " << u << ", state: (" << x0(0) << ", " << x0(1) << ")" << std::endl;
+  //      csv << u << "," << x0(0) <<"," << x0(1) << "\n";
+  //    }
+  //
+  //  }
+  std::cout << "\n";
+  std::cout << "========================" << std::endl;
+  std::cout << "Now executing first only" << std::endl;
+  std::cout << "========================" << std::endl;
+  {
+    std::ofstream csv("first_only.csv");
+    csv << "u,x0,x1\n";
+
+    Eigen::Vector2d x0{ M_PI, 0.0 };
+    std::cout << "Starting: (" << x0(0) << ", " << x0(1) << ")" << std::endl;
+
+    for (int i = 0; i < timesteps - 1; i++)
+    {
+      std::vector<double> us = getControl(x0);
+      double u = us[0];
+      u = std::clamp(u, -max_torque, max_torque);
+      x0 = propogate(x0, u);
+      std::cout << "Control: " << u << ", state: (" << x0(0) << ", " << x0(1) << ")" << std::endl;
+      csv << u << "," << x0(0) << "," << x0(1) << "\n";
+    }
+  }
 
   return 0;
 }
---
+++ src/viz/viz.py
@@ -0,0 +1,110 @@
+import pygame
+from pygame.locals import *
+from math import *
+import numpy as np
+import argparse
+
+COLOR = {'black': (0, 0, 0),
+         'red': (255, 0, 0),
+         'green': (0, 255, 0),
+         'blue': (0, 0, 255),
+         'white': (255, 255, 255)}
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 800
+SCREEN_DIM = (SCREEN_WIDTH, SCREEN_HEIGHT)
+SCREEN_CENTER = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+
+
+class Pendulum(pygame.sprite.Sprite):
+    def __init__(self, pivot_vect, length, bob_radius, data):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.font = pygame.font.SysFont("Ubuntu", 24)
+
+        self.length = length
+        self.bob_radius = bob_radius
+        self.index = 0
+        self.data = data
+
+        swinglen = length + bob_radius
+
+        self.image = pygame.Surface((swinglen * 2, swinglen * 2)).convert()
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (pivot_vect[0] - swinglen, pivot_vect[1] - swinglen)  # place so that pivot is at center
+
+        self.bob_x = int(length * sin(self._angle()) + self.rect.width // 2)
+        self.bob_y = int(length * cos(self._angle()) + self.rect.height // 2)
+        self.bob_rect = None
+
+        self._render()
+
+    def _render(self):
+        self.image.fill(COLOR['black'])
+        bob_pos = (self.bob_x, self.bob_y)
+
+        pygame.draw.aaline(self.image, COLOR['red'], (self.rect.width // 2, self.rect.height // 2), bob_pos, True)
+
+        self.bob_rect = pygame.draw.circle(self.image, COLOR['blue'], bob_pos, self.bob_radius, 0)
+        x1, y1 = self.bob_rect.topleft
+        x2, y2 = self.rect.topleft
+
+        self.bob_rect.topleft = (x1 + x2, y1 + y2)
+
+        text = self.font.render(f"{self._angle()}", True, COLOR['white'])
+        self.image.blit(text, (0, 0))
+
+    def _angle(self):
+        return self.data[self.index][1] + pi
+
+    def update(self):
+        # coords relative to pivot
+        self.index = (self.index + 1) % len(self.data)
+        angle = self._angle()
+        length = self.length
+
+        x = int(length * sin(angle))
+        y = int(length * cos(angle))
+        self.bob_x = x + self.rect.width // 2
+        self.bob_y = y + self.rect.height // 2
+        self._render()
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename", help="location of csv containing simulation data")
+    args = parser.parse_args()
+
+    if not args.filename:
+        return
+
+    data = np.genfromtxt(args.filename, delimiter=',', skip_header=1)
+
+    pygame.init()
+    screen = pygame.display.set_mode(SCREEN_DIM)
+    pygame.display.set_caption("Pendulum Simulation")
+
+    background = pygame.Surface(screen.get_size())
+    background = background.convert()
+    background.fill(COLOR['black'])
+    clock = pygame.time.Clock()
+
+    pendulum = Pendulum(pivot_vect=SCREEN_CENTER, length=300, bob_radius=25, data=data)
+    group = pygame.sprite.RenderPlain((pendulum,))
+
+
+    while True:
+        clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                return
+            elif event.type == KEYDOWN and (event.key == K_ESCAPE or event.key == K_q):
+                return
+        group.update()
+        screen.blit(background, (0, 0))
+        group.draw(screen)
+        pygame.display.flip()
+
+
+if __name__ == '__main__':
+    main()
